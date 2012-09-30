70d26f23230e84c94bb4ce15209cc374c7c94eae
Karsten Dambekalns <karsten@typo3.org>
2012-09-30 11:06:44 +0200

    [TASK] Adjust to FLOW3 renamed to TYPO3 Flow
    Adjusts the code, comments, documentation as needed.
    
    Change-Id: If633e2fb5407491843f283f7c394310ebae454fd
    Related: #41466
    Migration: TYPO3.Flow-201209251426

--- Classes/TYPO3/Imagine/AbstractImagineFactory.php
+++ Classes/TYPO3/Imagine/AbstractImagineFactory.php
@@ -2,7 +2,7 @@
 namespace TYPO3\Imagine;
 
 /*                                                                        *
- * This script belongs to the FLOW3 package "TYPO3.Imagine".              *
+ * This script belongs to the TYPO3 Flow package "Imagine".               *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
@@ -11,12 +11,12 @@ namespace TYPO3\Imagine;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use TYPO3\FLOW3\Annotations as FLOW3;
+use TYPO3\Flow\Annotations as Flow;
 
 /**
  * Abstract factory for Imagine package
  *
- * @FLOW3\Scope("singleton")
+ * @Flow\Scope("singleton")
  */
 class AbstractImagineFactory {
 
