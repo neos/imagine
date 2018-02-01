ba8ab24a769e91563b68975dfc4e56843beaa605
Christian Müller <christian@flownative.com>
2018-02-01 09:50:39 +0100

    Add fixme information about hotfix

--- Classes/AbstractImagineFactory.php
+++ Classes/AbstractImagineFactory.php
@@ -37,6 +37,7 @@ class AbstractImagineFactory
     {
         $this->settings = $settings;
         if (!isset($settings['enabledDrivers'])) {
+            // FIXME: This is a hotfix and should actually be fixed in the Neos setup step. As soon as it is fixed there, this condition can be removed.
             return;
         }
         if (!in_array($settings['driver'], array_keys(array_filter($settings['enabledDrivers'])), true)) {
