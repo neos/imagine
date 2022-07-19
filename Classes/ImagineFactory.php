df7b039e49d10c9ef33d03a8c91979f34d334bf2
Christian Müller <christian@flownative.com>
2022-07-19 12:56:42 +0200

    TASK: Refactor ImagineFactory
    Allows to check for driver existence and instantiation of any
    imagine class.

--- Classes/ImagineFactory.php
+++ Classes/ImagineFactory.php
@@ -11,6 +11,7 @@ namespace Neos\Imagine;
  * source code.
  */
 
+use Imagine\Image\ImagineInterface;
 use Neos\Flow\Annotations as Flow;
 
 /**
@@ -37,31 +38,76 @@ class ImagineFactory extends AbstractImagineFactory
      * \Imagine\Gd\Image.
      *
      * @param string $className If specified, this factory will create an instance of the driver dependent class
+     * @param mixed ...$arguments
+     * @return \Imagine\Image\ImagineInterface This is not necessarily true depending if you give another className
+     * @api
+     * @deprecated Use createDriver or createImagineClass as needed
+     * @see createDriver
+     * @see createImagineClass
+     */
+    public function create($className = 'Imagine', ...$arguments)
+    {
+        $this->configureDriverSpecificSettings($this->settings['driver']);
+        return $this->createImagineClass($this->settings['driver'], $className, ...$arguments);
+    }
+
+    /**
+     * Factory method to create a ImagineInterface driver instance, if you just want the currently configured one,
+     * inject the \Imagine\Image\ImagineInterface which will use this factory method with the configured driver class.
+     *
+     * This will also apply driver specific settings.
+     *
+     * @param string $driverName
+     * @param string $className
+     * @param mixed ...$arguments
      * @return \Imagine\Image\ImagineInterface
-     * @throws \ReflectionException
      * @api
      */
-    public function create($className = 'Imagine')
+    public function createDriver(string $driverName, ...$arguments): ImagineInterface
     {
-        $this->configureDriverSpecificSettings();
+        $this->configureDriverSpecificSettings($driverName);
+        $className = $this->buildImagineClassName($driverName);
 
-        $className = 'Imagine\\' . $this->settings['driver'] . '\\' . $className;
-        $arguments = array_slice(func_get_args(), 1);
+        return new $className(...$arguments);
+    }
 
-        switch (count($arguments)) {
-            case 0: $object = new $className(); break;
-            case 1: $object = new $className($arguments[0]); break;
-            case 2: $object = new $className($arguments[0], $arguments[1]); break;
-            case 3: $object = new $className($arguments[0], $arguments[1], $arguments[2]); break;
-            case 4: $object = new $className($arguments[0], $arguments[1], $arguments[2], $arguments[3]); break;
-            case 5: $object = new $className($arguments[0], $arguments[1], $arguments[2], $arguments[3], $arguments[4]); break;
-            case 6: $object = new $className($arguments[0], $arguments[1], $arguments[2], $arguments[3], $arguments[4], $arguments[5]); break;
-            default:
-                $class = new \ReflectionClass($className);
-                $object =  $class->newInstanceArgs($arguments);
-        }
+    /**
+     * Create an instance of a driver-dependent imagine class. For example, specifying
+     * "Image" with a driverName "Gd" will return an instance of the class
+     * \Imagine\Gd\Image.
+     *
+     * This will not set any driver specific settings.
+     *
+     * @param string $driverName
+     * @param string $className
+     * @param mixed ...$arguments
+     * @return object
+     * @api
+     */
+    public function createImagineClass(string $driverName, $className, ...$arguments): object
+    {
+        $className = $this->buildImagineClassName($driverName, $className);
+        return new $className(...$arguments);
+    }
 
-        return $object;
+    /**
+     * @param string $driverName
+     * @return bool
+     * @api
+     */
+    public function isDriverAvailable(string $driverName): bool
+    {
+        return class_exists($this->buildImagineClassName($driverName));
+    }
+
+    /**
+     * @param string $driverName
+     * @param string $className This should be a class that any imagine implementation provides, and defaults to the primary driver className
+     * @return string
+     */
+    private function buildImagineClassName(string $driverName, $className = 'Imagine'): string
+    {
+        return 'Imagine\\' . $driverName . '\\' . $className;
     }
 
     /**
@@ -69,9 +115,9 @@ class ImagineFactory extends AbstractImagineFactory
      *
      * @return void
      */
-    protected function configureDriverSpecificSettings()
+    protected function configureDriverSpecificSettings(string $driverName)
     {
-        if ($this->settings['driver'] === 'Imagick') {
+        if ($driverName === 'Imagick') {
             $this->configureImagickSettings();
         }
     }
